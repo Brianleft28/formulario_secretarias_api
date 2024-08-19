61080979d412d55e000fd8493099c12fb6413701
Brian Benegas <bbenegas@miituzaingo.gob.ar>
2024-08-19 12:05:27 -0300

    Update: server problema con los CORS

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import router from './routes/index';
-import Server from './models/Server';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import router from "./routes/index";
+import Server from "./models/Server";
 
 const server = new Server();
 
 const app = express();
 
-server.listen();
 server.useMiddlewares();
+server.app.use(cors());
 app.use(express.json());
-app.use(cors());
 server.useRouter(router);
 
-
-
+// Iniciar el servidor
+server.listen();
--- src/models/Server.ts
+++ src/models/Server.ts
@@ -1,31 +1,28 @@
-import express, {  Application, Router } from 'express';
+import express, { Application, Router } from "express";
 
 class Server {
+  public app: Application;
+  private port: string;
 
-    private app: Application;
-    private port: string;
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || "3000";
+  }
 
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT || '3000';
-    }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running at http://localhost:${this.port}`);
+    });
+  }
 
-   
-    listen(){
-        this.app.listen(this.port, ()=> {
-            console.log(`Server is running at http://localhost:${this.port}`);
-        })
-    }
-  
-    useMiddlewares(){
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: true}));
-    }
-   
-    useRouter(router: Router){
-        this.app.use(router);
-    }
+  useMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
 
+  useRouter(router: Router) {
+    this.app.use(router);
+  }
 }
 
 export default Server;
